1fc9a522ab9cd9095d86e72d558d759bbb3e58f1
the1nekev <kevcorcino15@gmail.com>
2023-03-28 19:29:22 -0400

    PokeDex app v1 (Beta)
    Adding 3 Dex pages for testing in console.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,26 @@
-alert('Hello world');
+//Empty Array for PokeDex
+const pokemonList = [];
 
-let favoriteFood = 'Pasta';
-document.write(favoriteFood);
+//Adding the first PokeDex "page"
+pokemonList[0] = {
+    name: "Heracross",
+    height: "1.5 m",
+    types: ["Bug", "Fighting"]
+};
+
+//Second PokeDex "page"
+pokemonList[1] = {
+    name: "Lucario",
+    height: "1.2 m",
+    types: ["Fighting", "Steel"]
+};
+
+//Third "page"
+pokemonList[2] = {
+    name: "Squirtle",
+    height: "0.5 m",
+    types: ["Water"]
+};
+
+//Output for testing PokeDex "pages"
+console.log(pokemonList);
