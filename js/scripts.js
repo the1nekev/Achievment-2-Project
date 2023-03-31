250eb1628030e001d1e5d435fa916041f31fb929
the1nekev <kevcorcino15@gmail.com>
2023-03-31 14:01:54 -0400

    Big or Small Function
    Added functionality to iterate through pokemonList. added special text to indicate small and big pokemon when an obejct is "called"

--- js/scripts.js
+++ js/scripts.js
@@ -4,23 +4,36 @@ const pokemonList = [];
 //Adding the first PokeDex "page"
 pokemonList[0] = {
     name: "Heracross",
-    height: "1.5 m",
+    height: 1.5 , // changed the date type for height from 'string' to 'integer'
     types: ["Bug", "Fighting"]
 };
 
 //Second PokeDex "page"
 pokemonList[1] = {
     name: "Lucario",
-    height: "1.2 m",
+    height: 1.2,
     types: ["Fighting", "Steel"]
 };
 
 //Third "page"
 pokemonList[2] = {
     name: "Squirtle",
-    height: "0.5 m",
+    height: 0.5,
     types: ["Water"]
 };
 
 //Output for testing PokeDex "pages"
 console.log(pokemonList);
+
+//For Loop to iterate through PokeDex
+for (let i = 0; i < pokemonList.length; i++){
+    document.write(pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")" + " ");
+    //conditional to check pokemon height
+    if (pokemonList[i].height > 1.3){
+       document.write("-Wow that's a big Pokemon");
+    } else {
+        document.write(" -Little Man!")
+    }
+    document.write("<br>")
+
+};
