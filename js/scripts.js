7e1c5fa44384f6163e0ab7a67dd187388224af32
the1nekev <kevcorcino15@gmail.com>
2023-04-14 17:47:58 -0400

    eventListener & Validation
    added if statement to validate data type to 'object',
    
    added styling to html doc.
    
    added eventListener to capture click on buttons and log name to the console.

--- js/scripts.js
+++ js/scripts.js
@@ -27,18 +27,48 @@ let pokemonRepository = (function () {
     }
 
     //add() funtion to add item to pokemonList
-    function add(){
-        pokemonList.push(pokemon);
+    function add(pokemon){
+        //Validation of proper data input
+        if (
+            typeof pokemon === "object" &&
+            "name" in pokemon &&
+            "height" in pokemon &&
+            "types" in pokemon
+        ) {
+            pokemonList.push(pokemon);
+        }else{
+            console.log("failed to add pokemon")
+        }     
     }
-
+    
+    function addListItem(pokemon){
+        let pokemonList = document.querySelector(".pokemon-list");
+        let listPokemon = document.createElement("li");
+        let button = document.createElement("button"); 
+        button.innerText = pokemon.name;
+        button.classList.add("button-class");
+        listPokemon.appendChild(button);
+        pokemonList.appendChild(listPokemon);
+        button.addEventListener("click", () => showDetails(pokemon));
+    };
+    function showDetails(pokemon){
+        console.log(pokemon.name);
+    };
     return{
         add: add,
-        getAll: getAll
+        getAll: getAll,
+        addListItem: addListItem,
     };
+    
 
 }) ();
+
+//adding more pokemon for test
+//pokemonRepository.add({ name: "Pikachu", height: 0.3, types:["electric"]});
 //console.log(pokemonRepository.getAll());
 
-
 //forEach Loop to iterate through PokeDex
-pokemonRepository.getAll().forEach(pokemonList => console.log(pokemonList));
+pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+});
+
